4d2a2485b8c5011d2846a4a83960d752c4c28d2d
Juan Manuel Martinez <vagrant@homestead>
2017-06-21 21:31:06 +0000

    Commit inicial

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('correo')->unique();
+            $table->string('password');
+            $table->integer('codigo')->unique()->unsigned();
+            $table->enum('rol',['Admin','Prestador']);
+            $table->integer('carrera_id')->unsigned();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
